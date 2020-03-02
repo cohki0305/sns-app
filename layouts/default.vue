a6b6fb207a37c992c12b67e966713986a8cd4d0f
cohki0305 <cohki0305@gmail.com>
2020-03-02 15:56:21 +0900

    UIを作成

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,20 @@
 <template>
   <div>
     <nuxt />
+    <app-footer />
   </div>
 </template>
+
+<script>
+import AppFooter from '~/components/Footer.vue'
+
+export default {
+  components: {
+    AppFooter
+  }
+}
+
+</script>
 
 <style>
 html {
@@ -22,5 +34,33 @@ html {
   display: flex;
   flex-direction: column;
   justify-content: center;
+}
+
+.modal {
+  backgroud: white;
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  top: 0;
+}
+
+.uploaded-image {
+  max-height: 120px;
+}
+
+.post-btn {
+  padding: 4px 12px;
+  border-radius: 24px;
+  backgroud-color: black;
+  color: white;
+  font-size: 12px;
+}
+
+.el-upload {
+  width: 100%;
+}
+
+.el-button {
+  width: 100%;
 }
 </style>
