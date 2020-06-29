1d6e31d0f2eef2c0615311823824f2d90089a4a1
cohki0305 <cohki0305@gmail.com>
2020-06-29 10:29:07 +0900

    フォロー機能

--- components/User.vue
+++ components/User.vue
@@ -11,13 +11,21 @@
      </div>
    </div>
    <div class="follow-btn vertical-middle" v-if="!isCurrentUser">
-     <el-button>Follow</el-button>
+     <el-button v-if="!followed" @click="follow">Follow</el-button>
+     <el-button v-else>Unfollow</el-button>
    </div>
  </div>
 </template>
 
 <script>
+import { db } from '~/plugins/firebase'
+
 export default {
+  data () {
+    return {
+      followed: false
+    }
+  },
   props: ['user'],
   computed: {
     currentUser () {
@@ -25,6 +33,13 @@ export default {
     },
     isCurrentUser () {
       return this.currentUser.uid === this.user.id
+    }
+  },
+  methods: {
+    async follow () {
+      await db.collection('users').doc(this.currentUser.uid).collection('followings').doc(this.user.id).set({ user: this.user.id })
+      await db.collection('users').doc(this.user.id).collection('followers').doc(this.currentUser.uid).set({ user: this.currentUser.uid })
+      this.followed = true
     }
   }
 }
